b7be7e93c3251dafaaec7e36d4df19a6a4a86558
alperergul <alper.ergul.97@gmail.com>
2023-10-22 02:43:54 +0300

    initial couter added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CounterbuttonComponent } from './component/counterbutton/counterbutton.component';
+import { CounterdisplayComponent } from './component/counterdisplay/counterdisplay.component';
+import { StoreModule } from '@ngrx/store';
+import { counterReducer } from './shared/store/counter.reducer';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './Material.Module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, CounterbuttonComponent, CounterdisplayComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule,
+    StoreModule.forRoot({ counter: counterReducer }),
+    BrowserAnimationsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
